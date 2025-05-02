64dcaa29df73c0fde6ffbdd25f99d4d6912a70aa
Mst. Shahena Akter <sa4389310@gmail.com>
2025-05-02 16:59:48 +0600

    fetch api use loop

--- script.js
+++ script.js
@@ -6,36 +6,88 @@
 // .then(response => response.json())
 // .then(data =>console.log(data))
 
-fetch("https://jsonplaceholder.typicode.com/comments")
-.then(response => response.json())
-.then(posts => console.log(posts))
+// posts
+// fetch("https://jsonplaceholder.typicode.com/comments")
+// .then(response => response.json())
+// .then(posts => console.log(posts))
 
-fetch("https://jsonplaceholder.typicode.com/comments")
-.then(response => response.json())
-.then(comments =>{
-    console.log(comments)
-})
+// // comments
+// fetch("https://jsonplaceholder.typicode.com/comments")
+// .then(response => response.json())
+// .then(comments =>{
+//     console.log(comments)
+// })
 
-fetch("https://jsonplaceholder.typicode.com/albums")
-.then(response => response.json())
-.then(albums =>{
-    console.log(albums)
-})
+// // albums
+// fetch("https://jsonplaceholder.typicode.com/albums")
+// .then(response => response.json())
+// .then(albums =>{
+//     console.log(albums)
+// })
 
-fetch("https://jsonplaceholder.typicode.com/photos")
-.then(response => response.json())
-.then( photos =>{
-    console.log(photos)
-})
+// // photos
+// fetch("https://jsonplaceholder.typicode.com/photos")
+// .then(response => response.json())
+// .then( photos =>{
+//     console.log(photos)
+// })
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-.then(response => response.json())
-.then(todos =>{
-    console.log(todos)
-})
+// // todos
+// fetch("https://jsonplaceholder.typicode.com/todos")
+// .then(response => response.json())
+// .then(todos =>{
+//     console.log(todos)
+// })
 
-fetch("https://jsonplaceholder.typicode.com/users")
+// // users
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(response => response.json())
+// .then(users =>{
+//     console.log(users)
+// })
+
+
+
+
+
+
+
+
+
+let data = document.getElementById("data")
+
+// // 1.step
+// let url1 = "https://jsonplaceholder.typicode.com/posts"
+// fetch(url1)
+// .then(response => response.json())
+// .then(posts =>{
+// console.log(posts)
+//  data.innerHTML = posts[0].title
+
+// })
+
+// // 2.step
+// let url2 = "https://jsonplaceholder.typicode.com/posts"
+// fetch(url2)
+// .then(response => response.json())
+// .then(posts =>{
+//     let li = document.createElement("li")
+//     li.innerHTML = posts[0].title
+//     data.appendChild(li)
+
+// })
+
+// 3.step
+let url3 = "https://jsonplaceholder.typicode.com/posts"
+fetch(url3)
 .then(response => response.json())
-.then(users =>{
-    console.log(users)
+.then(posts =>{
+  posts.forEach(datas => {
+
+    let li  = document.createElement("li")
+    li.innerHTML = datas.title
+    data.appendChild(li)
+    
+  });
+
 })
