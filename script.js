257a99af0653cca80f000085945a0ee8b2f5b611
Mst. Shahena Akter <sa4389310@gmail.com>
2025-05-02 16:09:16 +0600

    upload

--- script.js
+++ script.js
@@ -8,4 +8,34 @@
 
 fetch("https://jsonplaceholder.typicode.com/comments")
 .then(response => response.json())
-.then(data => console.log(data))
+.then(posts => console.log(posts))
+
+fetch("https://jsonplaceholder.typicode.com/comments")
+.then(response => response.json())
+.then(comments =>{
+    console.log(comments)
+})
+
+fetch("https://jsonplaceholder.typicode.com/albums")
+.then(response => response.json())
+.then(albums =>{
+    console.log(albums)
+})
+
+fetch("https://jsonplaceholder.typicode.com/photos")
+.then(response => response.json())
+.then( photos =>{
+    console.log(photos)
+})
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then(response => response.json())
+.then(todos =>{
+    console.log(todos)
+})
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(users =>{
+    console.log(users)
+})
